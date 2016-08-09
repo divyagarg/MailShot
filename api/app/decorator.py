b54e704f5e83defb11caf4b7f3f2a9d81089073d
ruchimann <ruchimann87@gmail.com>
2016-08-09 14:56:25 +0530

    adding status in son decorator

--- api/app/decorator.py
+++ api/app/decorator.py
@@ -1,14 +1,24 @@
 import functools
-import ujson
+import logging
 
+import ujson
 from flask import Response
+
+logger = logging.getLogger()
 
 
 def json(f):
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
         rv = f(*args, **kwargs)
-        rv = ujson.dumps(rv)
-        return Response(rv, mimetype='application/json')
+        resp = dict()
+        resp["data"] = rv
+        if isinstance(rv, Exception):
+            resp["status"] = False
+        else:
+            resp["status"] = True
+        logger.debug(resp)
+        resp = ujson.dumps(resp)
+        return Response(resp, mimetype='application/json')
 
     return wrapped
