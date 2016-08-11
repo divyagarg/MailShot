b99e5c5b9eca883925019e1c983a128270b45582
ruchimann <ruchimann87@gmail.com>
2016-08-11 16:10:02 +0530

    changing logger handler

--- api/app/log.py
+++ api/app/log.py
@@ -3,15 +3,15 @@ import logging.config
 from config import LOG_FILE, LOG_FILE_ERROR
 
 
+
 def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    handler = logging.handlers.WatchedFileHandler(LOG_FILE)
+    handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
-    errorhandler = logging.handlers.WatchedFileHandler(LOG_FILE_ERROR)
+    errorhandler = logging.StreamHandler()
     errorhandler.setLevel(logging.ERROR)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(funcName)s-[in %(pathname)s:%(lineno)d]- %(message)s")
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(funcName)s-[in %(pathname)s:%(lineno)d]- %(message)s")
     handler.setFormatter(formatter)
     errorhandler.setFormatter(formatter)
     logger.addHandler(handler)
